1cb403b14afce5624bd96ed0316d37dda09d7831
Nicolás Moreno Posada <nicolas@192.168.0.9>
2020-08-17 14:24:03 -0500

    Updates

---
+++ precache-manifest.73ab7fd5b7576ca5840cd869d00cd7c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817a907b703914de4304df5f8b4fcc80",
+    "url": "/react-deploy-test/index.html"
+  },
+  {
+    "revision": "2f1ac5187473322215ea",
+    "url": "/react-deploy-test/static/css/main.fc22a8a2.chunk.css"
+  },
+  {
+    "revision": "c81194e024c7abf269d1",
+    "url": "/react-deploy-test/static/js/2.2ba8455b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy-test/static/js/2.2ba8455b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f1ac5187473322215ea",
+    "url": "/react-deploy-test/static/js/main.b6feddb5.chunk.js"
+  },
+  {
+    "revision": "d3e2f413c23f27275079",
+    "url": "/react-deploy-test/static/js/runtime-main.4964af8a.js"
+  },
+  {
+    "revision": "f551d7f8d49ec73d8a894c23cb0fd4df",
+    "url": "/react-deploy-test/static/media/pp.f551d7f8.jpg"
+  }
+]);
